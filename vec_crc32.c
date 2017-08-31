b64bd9942a27f985b4e92eb933c948c668865c86
Daniel Black <daniel.black@au.ibm.com>
2017-08-31 11:12:14 +1000

    Fold vec_crc32_wrapper.c into vec_crc32.c
    Signed-off-by: Daniel Black <daniel.black@au.ibm.com>

--- vec_crc32.c
+++ vec_crc32.c
@@ -32,8 +32,71 @@
 #include <altivec.h>
 
 #define POWER8_INTRINSICS
+#define CRC_TABLE
 
 #include "crc32_constants.h"
+
+#define VMX_ALIGN	16
+#define VMX_ALIGN_MASK	(VMX_ALIGN-1)
+
+#ifdef REFLECT
+static unsigned int crc32_align(unsigned int crc, unsigned char *p,
+			       unsigned long len)
+{
+	while (len--)
+		crc = crc_table[(crc ^ *p++) & 0xff] ^ (crc >> 8);
+	return crc;
+}
+#else
+static unsigned int crc32_align(unsigned int crc, unsigned char *p,
+				unsigned long len)
+{
+	while (len--)
+		crc = crc_table[((crc >> 24) ^ *p++) & 0xff] ^ (crc << 8);
+	return crc;
+}
+#endif
+
+static unsigned int __attribute__ ((aligned (32)))
+__crc32_vpmsum(unsigned int crc, void* p, unsigned long len);
+
+unsigned int crc32_vpmsum(unsigned int crc, unsigned char *p,
+			  unsigned long len)
+{
+	unsigned int prealign;
+	unsigned int tail;
+
+#ifdef CRC_XOR
+	crc ^= 0xffffffff;
+#endif
+
+	if (len < VMX_ALIGN + VMX_ALIGN_MASK) {
+		crc = crc32_align(crc, p, len);
+		goto out;
+	}
+
+	if ((unsigned long)p & VMX_ALIGN_MASK) {
+		prealign = VMX_ALIGN - ((unsigned long)p & VMX_ALIGN_MASK);
+		crc = crc32_align(crc, p, prealign);
+		len -= prealign;
+		p += prealign;
+	}
+
+	crc = __crc32_vpmsum(crc, p, len & ~VMX_ALIGN_MASK);
+
+	tail = len & VMX_ALIGN_MASK;
+	if (tail) {
+		p += len & ~VMX_ALIGN_MASK;
+		crc = crc32_align(crc, p, tail);
+	}
+
+out:
+#ifdef CRC_XOR
+	crc ^= 0xffffffff;
+#endif
+
+	return crc;
+}
 
 /* When we have a load-store in a single-dispatch group and address overlap
  * such that foward is not allowed (load-hit-store) the group must be flushed.
@@ -68,7 +131,7 @@ static const __vector unsigned long vperm_const
 			0x0F0E0D0C0B0A0908UL };
 #endif
 
-unsigned int __attribute__ ((aligned (32)))
+static unsigned int __attribute__ ((aligned (32)))
 __crc32_vpmsum(unsigned int crc, void* p, unsigned long len) {
 
 	const __vector unsigned long vzero = {0,0};
