bdf3d28159641959c59c63286452621221186935
David Zeng <zengxiaoh81@gmail.com>
2018-01-10 11:02:04 +0800

    Fix compiling warning.
    Signed-off-by: David Zeng <zengxiaoh81@gmail.com>

--- vec_crc32.c
+++ vec_crc32.c
@@ -64,6 +64,8 @@ static unsigned int crc32_align(unsigned int crc, unsigned char *p,
 static unsigned int __attribute__ ((aligned (32)))
 __crc32_vpmsum(unsigned int crc, void* p, unsigned long len);
 
+unsigned int crc32_vpmsum(unsigned int crc, unsigned char *p,
+                unsigned long len);
 #ifndef CRC32_FUNCTION
 #define CRC32_FUNCTION  crc32_vpmsum
 #endif
@@ -181,8 +183,8 @@ __crc32_vpmsum(unsigned int crc, void* p, unsigned long len) {
 	unsigned int result = 0;
 	unsigned int offset; /* Constant table offset. */
 
-	long i; /* Counter. */
-	long chunks;
+	unsigned long i; /* Counter. */
+	unsigned long chunks;
 
 	unsigned long block_size;
 	int next_block = 0;
@@ -252,8 +254,8 @@ __crc32_vpmsum(unsigned int crc, void* p, unsigned long len) {
 
 		/* xor in initial value */
 		vdata0 = vec_xor(vdata0, vcrc);
-
-		p += 128;
+		
+		p = (char *)p + 128;
 
 		do {
 			/* Checksum in blocks of MAX_SIZE. */
@@ -321,14 +323,14 @@ __crc32_vpmsum(unsigned int crc, void* p, unsigned long len) {
 				vdata7 = vec_ld(112, (__vector unsigned long*) p);
 				VEC_PERM(vdata7, vdata7, vdata7, vperm_const);
 
-				p += 128;
+				p = (char *)p + 128;
 
 				/*
 				 * main loop. We modulo schedule it such that it takes three
 				 * iterations to complete - first iteration load, second
 				 * iteration vpmsum, third iteration xor.
 				 */
-				for (i = 0; i < chunks-2; i++, p += 128) {
+				for (i = 0; i < chunks-2; i++) {
 					vconst1 = vec_ld(offset, vcrc_const);
 					offset += 16;
 					GROUP_ENDING_NOP;
@@ -389,6 +391,8 @@ __crc32_vpmsum(unsigned int crc, void* p, unsigned long len) {
 							long)vdata7, (__vector unsigned long long)vconst1);
 					vdata7 = vec_ld(112, (__vector unsigned long long*) p);
 					VEC_PERM(vdata7, vdata7, vdata7, vperm_const);
+
+					p = (char *)p + 128;
 				}
 
 				/* First cool down*/
@@ -495,7 +499,7 @@ __crc32_vpmsum(unsigned int crc, void* p, unsigned long len) {
 			va7 = vec_ld(112, (__vector unsigned long*) p);
 			VEC_PERM(va7, va7, va7, vperm_const);
 
-			p += 128;
+			p = (char *)p + 128;
 
 			vdata0 = vec_xor(v0, va0);
 			vdata1 = vec_xor(v1, va1);
